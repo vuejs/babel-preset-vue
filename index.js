2ddda53b46cc8e8500734a2401e86f35207fe5ed
EGOIST <0x142857@gmail.com>
2018-01-30 17:06:49 +0800

    make babel 7 happy

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import VueJSX from 'babel-plugin-transform-vue-jsx'
 import JsxEventModifiers from 'babel-plugin-jsx-event-modifiers'
 import JsxVModel from 'babel-plugin-jsx-v-model'
 
-export default {
+export default () => ({
   plugins: [
     JsxEventModifiers,
     JsxVModel,
     VueJSX
   ]
-}
+})
